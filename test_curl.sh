77784d3ce75b675a6cd33aff5cd8a968e462482f
Sven Westerlaken <sven.westerlaken@hotmail.nl>
2018-08-21 11:41:18 +0200

    add sudo to curl

--- test_curl.sh
+++ test_curl.sh
@@ -1,1 +1,1 @@
-curl -H 'Host: spotifinest.svenwesterlaken.nl' http://localhost && curl -H 'Host: spotifinest-api.svenwesterlaken.nl' http://localhost
+sudo curl -H 'Host: spotifinest.svenwesterlaken.nl' http://localhost && sudo curl -H 'Host: spotifinest-api.svenwesterlaken.nl' http://localhost
