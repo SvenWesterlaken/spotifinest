d6fec95e3be8d39307072b2be51a10239ca7582b
Sven Westerlaken <sven.westerlaken@hotmail.nl>
2020-04-23 21:19:41 +0200

    update api url

--- app/config/prod.env.js
+++ app/config/prod.env.js
@@ -4,5 +4,5 @@ module.exports = {
   TIME_SHORT: '"short_term"',
   TIME_MEDIUM: '"medium_term"',
   TIME_LONG: '"long_term"',
-  API_BASE_URL: '"http://spotifinest-api.svenwesterlaken.nl"'
+  API_BASE_URL: '"https://api.spotifinest.svenwesterlaken.nl"'
 }
